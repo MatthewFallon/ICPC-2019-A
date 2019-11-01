0590c4212c0391e3a619ad368f4bafd4d9bede67
Matthew Fallon <mfallon@grapefruit-lacroix.hsd1.il.comcast.net>
2019-11-01 01:01:39 -0500

    Created Vector input and output traversal scripts.

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main(__attribute__((unused)) int argc,__attribute__((unused)) char const *argv[]) {
-  int length;
+  long length;
   cin >> length;
-  cout << length << '\n';
+  std::vector< std::vector<int> > rows( 4, std::vector<int>(length) );
+  for (int i=0; i<4; i++) {
+    for (int j = 0; j < length; j++) {
+      cin >> rows.at(i).at(j);
+    }
+  }
+  for (auto row = rows.begin(); row != rows.end(); row++) {
+    auto col = *row;
+    std::cout << '\n';
+    for (auto el = col.begin(); el != col.end(); el++) {
+      std::cout << *el << ' ';
+    }
+  }
   return 0;
 }
